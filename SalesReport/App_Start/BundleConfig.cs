d10b255a74a71c743bd6aa4ffb00a8fe8e1c61ef
alex <sav.1209@gmail.com>
2016-05-22 17:07:12 +0500

    implemented bundles for project and installed needed libraries: jquery, bootstrap (+ datetimepicker), momentjs

---
+++ SalesReport/App_Start/BundleConfig.cs
@@ -0,0 +1,35 @@
+﻿namespace SalesReport
+{
+    using System.Web.Optimization;
+
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/jquery").Include(
+                "~/Scripts/jquery-1.9.1.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap").Include(
+                "~/Scripts/bootstrap.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/Scripts/moment").Include(
+                "~/Scripts/moment.js")
+                );
+
+            bundles.Add(new ScriptBundle("~/Scripts/bootstrap-datetimepicker").Include(
+                "~/Scripts/bootstrap-datetimepicker.js")
+                );
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-theme.css")
+                );
+
+            bundles.Add(new StyleBundle("~/Content/bootstrap-datetimepicker").Include(
+                "~/Content/bootstrap-datetimepicker.css")
+                );
+        }
+    }
+}
